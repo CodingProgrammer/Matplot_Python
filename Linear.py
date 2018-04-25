b76a53ec22f2f5897d124f388a0e15888694151f
Unknown <157136443@qq.com>
2018-04-25 11:16:04 +0800

    Linear

---
+++ Linear.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt 
+import numpy as np  
+import pandas as pd 
+plt.plot(range(1, 11), [301.5, 298.8, 302.4, 300.9, 299.6, 302.2, 301.6, 297.5, 302.2, 299.5], ':g') 
+plt.show() 
